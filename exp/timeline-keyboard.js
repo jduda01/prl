77f66eb20beaf77ec1c6be48aa506fa8d22e41f3
Jessica Duda <jessica.duda@yale.edu>
2026-01-25 13:58:05 -0500

    fixed timeline-keyboard (choice to choices) to enable key presses

--- exp/timeline-keyboard.js
+++ exp/timeline-keyboard.js
@@ -4,7 +4,7 @@
 const welcome = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: instructions[0],
-    choice: "NO_KEYS",
+    choices: "NO_KEYS", // changed from choice to choices 
     on_load: toggleDebugMode,
 };
 
